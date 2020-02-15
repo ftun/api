356712e7b7e9a07f0e8b0f950c3d7e63a2062232
ftun <felipe.tun.cauich@gmail.com>
2020-02-15 13:07:03 -0500

    mod

---
+++ app/controllers/ServicioController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Api\controllers;
+
+use Api\Helper\BController;
+use Api\models\DefServicio;
+
+/**
+ * @author Felipe Tun <felipe.tun.cauich@gmail.com.com>
+ * Herendan los siguientes metodos del BController
+ *      parent::index();
+ *      parent::search();
+ *      parent::searchPagination($limit, $offset);
+ *      parent::post();
+ *      parent::put($id);
+ */
+class ServicioController extends BController
+{
+    /**
+    * @property. Object
+    */
+    public $modelClass = "Api\\models\\DefServicio";
+
+    /**
+    * Funcion que obtienen informacion de un elemento del modelo en base a su ID
+    * @param /GET
+    * @return JSON
+    */
+    public function getElementById($id)
+    {
+        $sql = "SELECT * FROM {$this->modelClass::getSource()} WHERE iddef_servicio = :id;";
+        return $this->getResponseQueryOne($sql, ['id' => $id]);
+    }
+}
